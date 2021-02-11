faec9022d57f724d2bf768e717d3491a296abbe6
Brieuc Francois <brieucfrancois@hotmail.com>
2021-02-11 11:55:24 +0100

    Script to launch several energies in condor

---
+++ FCCSW_ecal/launch_several_energies.sh
@@ -0,0 +1,4 @@
+python condor_submit_fccsw.py -outputFolder /eos/user/b/brfranco/rootfile_storage/ -campaignName $(date +"%y%m%d")_energies_10kevt \
+ -gaudiConfig $PWD/runSlidingWindowAndCaloSim.py -jobType caloReco \
+ -energies 300 650 1000 2000 5000 10000 20000 30000 45000 100000 150000 200000 -polarAngles 55 -energiesForDifferentPolarAngles 10000 \
+ -nEvt 10000 -originalNjobs 1 --submit
