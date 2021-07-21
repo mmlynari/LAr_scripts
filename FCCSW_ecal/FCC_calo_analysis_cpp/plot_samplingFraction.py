294255794bbe3139b731096587d496aa83d9ed0f
Brieuc Francois <brieucfrancois@hotmail.com>
2021-07-21 15:08:16 +0200

    Forgot one sampling fraction to change v2

--- FCCSW_ecal/FCC_calo_analysis_cpp/plot_samplingFraction.py
+++ FCCSW_ecal/FCC_calo_analysis_cpp/plot_samplingFraction.py
@@ -144,7 +144,7 @@ for ifile, filename in enumerate(calo_init.filenamesIn):
 
 if calo_init.args.sed:
     command = "sed -i 's/samplingFraction =.*,/samplingFraction = %s,/' "%string_for_fccsw
-    os.system(command + " runSlidingWindowAndCaloSim.py") # it has to be launched from FCCSW_ecal folder
+    os.system(command + " run*AndCaloSim.py") # it has to be launched from FCCSW_ecal folder
     print(command + " run*AndCaloSim.py")
     os.system("sed -i 's/samplingFractions =.*,/samplingFractions = %s,/' fcc_ee_upstream_inclinedEcal.py"%string_for_fccsw) # it has to be launched from FCCSW_ecal folder
     os.system("sed -i 's/samplingFractions =.*,/samplingFractions = [%s],/' runUpstreamSlidingWindowAndCaloSim.py"%string_for_fccsw) # it has to be launched from FCCSW_ecal folder
