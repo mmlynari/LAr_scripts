df0e006b533837a1767d8b131d42b9d9bd6f8d65
Brieuc Francois <brieucfrancois@hotmail.com>
2021-08-12 18:30:47 +0200

    Automation for the calo size for the dead material correction

--- FCCSW_ecal/write_calibration_xml.py
+++ FCCSW_ecal/write_calibration_xml.py
@@ -1,11 +1,12 @@
 import os, sys
 from xml.dom import minidom
-# python write_calibration_xml.py ../../Detector/DetFCCeeECalInclined/compact/FCCee_ECalBarrel.xml
+# python write_calibration_xml.py ../../FCCDetectors/Detector/DetFCCeeECalInclined/compact/FCCee_ECalBarrel.xml
 # careful: if you use the FCC_DETECTOR environment variable, you have to recompile after modifying the xml so that they go in the right place
 
 input_xml_path = sys.argv[1]
 output_xml_path_sf = input_xml_path.replace(".xml", "_calibration.xml")
 output_xml_path_upstream = input_xml_path.replace(".xml", "_upstream.xml")
+detDim_xml_path = os.path.join(os.path.dirname(input_xml_path), "../../DetFCCeeIDEA-LAr/compact/FCCee_DectDimensions.xml")
 
 list_of_pair_layerThickness_numberOfLayer = []
 
@@ -14,9 +15,19 @@ input_xml = minidom.parse(input_xml_path)
 numberOfLayer = 0
 n_phi_bins = 0
 eta_bin_size = 0
+original_cryo_back_size = 0
 #print input_xml.getElementsByTagName('lccdd')
 for nodeList in input_xml.getElementsByTagName('lccdd'):
     for node in nodeList.childNodes:
+        #get the cryostat back size
+        if node.localName == 'define':
+            for subnode in node.childNodes:
+                if subnode.localName == 'constant' and subnode.getAttribute('name') == 'CryoThicknessBack':
+                    original_cryo_back_size_str = subnode.getAttribute('value')
+                    if not original_cryo_back_size_str.split("*")[1] == 'mm':
+                        print("Error in orignal xml file, cryo thickness expected in mm, exiting...")
+                        sys.exit(1)
+                    original_cryo_back_size = int(original_cryo_back_size_str.split("*")[0])
         if node.localName == 'detectors':
             for subnode in node.childNodes:
                 if subnode.localName == 'detector':
@@ -84,7 +95,25 @@ os.system("sed -i 's/deltaEtaTower.*$/deltaEtaTower = %s, deltaPhiTower = 2*_pi\
 print("run*SlidingWindowAndCaloSim.py modified")
 
 # Write upstream correction xml
-# Re-make absorber and readout not sensitive
+# Re-make absorber and readout not sensitive, make cryostat sensitive and anlarge it to 1.1 m 
+# First find the detector dimension to enlarge it for the new big cryostat 
+detDim_xml = minidom.parse(detDim_xml_path)
+BarECal_rmax = '0'
+for nodeList in detDim_xml.getElementsByTagName('lccdd'):
+    for node in nodeList.childNodes:
+        if node.localName == 'define':
+            for subnode in node.childNodes:
+                if subnode.localName == 'constant' and subnode.getAttribute('name') == 'BarECal_rmax':
+                    BarECal_rmax = subnode.getAttribute('value')
+
+
+new_cryo_back_size = 1100 #mm
+BarECal_rmax_int = int(BarECal_rmax.split('*')[0])
+if not BarECal_rmax.split('*')[1] == 'mm':
+    print("Error: dimensions from FCCee_DectDimensions.xml should be in mm, exiting...")
+    sys.exit(1)
+new_BarECal_rmax_int = BarECal_rmax_int + (new_cryo_back_size - original_cryo_back_size)
+
 added_rmax = False
 for nodeList in input_xml.getElementsByTagName('lccdd'):
     for node in nodeList.childNodes:
@@ -93,11 +122,11 @@ for nodeList in input_xml.getElementsByTagName('lccdd'):
                 if not added_rmax:
                     rMaxNode = input_xml.createElement('constant')
                     rMaxNode.setAttribute('name', 'BarECal_rmax')
-                    rMaxNode.setAttribute('value', '3700*mm')
+                    rMaxNode.setAttribute('value', '%d*mm'%new_BarECal_rmax_int)
                     node.insertBefore(rMaxNode, subnode)
                     added_rmax = True
                 if subnode.localName == 'constant' and subnode.getAttribute('name') == "CryoThicknessBack":
-                    subnode.setAttribute('value', "1100*mm")
+                    subnode.setAttribute('value', "%d*mm"%new_cryo_back_size)
         if node.localName == 'detectors':
             for subnode in node.childNodes:
                 if subnode.localName == 'detector':
