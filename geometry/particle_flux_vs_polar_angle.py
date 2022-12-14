24c400816145828eca08292308012c5aaef75018
BrieucF <brieucfrancois@hotmail.com>
2022-12-14 14:35:35 +0100

    Add flux vs z in particle_flux_vs_polar_angle.py

--- geometry/particle_flux_vs_polar_angle.py
+++ geometry/particle_flux_vs_polar_angle.py
@@ -1,6 +1,6 @@
 import os, sys
 import ROOT
-from math import cos, pi
+from math import cos, pi, tan
 
 ROOT.gROOT.SetBatch(True)
 
@@ -12,9 +12,9 @@ try:
 except:
     pass
 
-#input_file_path = "/eos/user/b/brfranco/rootfile_storage/delphes_ee_to_z_to_all/ee_z.root"
+input_file_path = "/eos/user/b/brfranco/rootfile_storage/delphes_ee_to_z_to_all/ee_z.root"
 #input_file_path = "/eos/user/b/brfranco/rootfile_storage/delphes_ee_to_z_to_all/ee_ee.root"
-input_file_path = "/eos/user/b/brfranco/rootfile_storage/delphes_ee_to_z_to_all/ee_aa.root"
+#input_file_path = "/eos/user/b/brfranco/rootfile_storage/delphes_ee_to_z_to_all/ee_aa.root"
 output_prefix = os.path.basename(input_file_path).split(".")[0]
 input_rootfile = ROOT.TFile(input_file_path)
 events = input_rootfile.Get("Delphes")
@@ -23,6 +23,9 @@ print(events.GetEntries())
 flux_vs_cosTheta = ROOT.TH1F("flux_vs_cosTheta", "flux_vs_cosTheta", 50, -1, 1)
 flux_vs_theta = ROOT.TH1F("flux_vs_theta", "flux_vs_theta", 50, 0, pi)
 flux_vs_eta = ROOT.TH1F("flux_vs_eta", "flux_vs_eta", 50, -5, 5)
+flux_vs_z = ROOT.TH1F("flux_vs_z", "flux_vs_z", 50, -300, 300)
+
+detector_inner_radius = 216
 
 n_particles = 0
 for event in events:
@@ -38,6 +41,7 @@ for event in events:
                 flux_vs_cosTheta.Fill(cos(p4.Theta()))
                 flux_vs_theta.Fill(p4.Theta())
                 flux_vs_eta.Fill(p4.Eta())
+                flux_vs_z.Fill(detector_inner_radius/tan(p4.Theta()))
                 n_particles += 1
 
 print("Number of particles accepted: ", n_particles)
@@ -78,3 +82,10 @@ flux_vs_eta.GetYaxis().SetTitle("#Particles")
 flux_vs_eta.GetXaxis().SetTitle("#eta")
 flux_vs_eta.Draw()
 eta_canvas.Print("eta_canvas_" + output_prefix + ".png")
+
+z_canvas = ROOT.TCanvas("z_canvas", "z_canvas")
+flux_vs_z.SetTitle("")
+flux_vs_z.GetYaxis().SetTitle("#Particles")
+flux_vs_z.GetXaxis().SetTitle("z (cm)")
+flux_vs_z.Draw()
+z_canvas.Print("z_canvas_" + output_prefix + ".png")
