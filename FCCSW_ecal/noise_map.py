ebcf565812f79234a7fea913d2543d01b1c533a6
Giovanni Marchiori <giovanni.marchiori@cern.ch>
2023-09-13 15:29:47 +0200

    update scripts to create neighbours map and noise map

--- FCCSW_ecal/noise_map.py
+++ FCCSW_ecal/noise_map.py
@@ -1,33 +1,39 @@
 from Gaudi.Configuration import *
-
 # Detector geometry
 from Configurables import GeoSvc
 geoservice = GeoSvc("GeoSvc")
 # if FCC_DETECTORS is empty, this should use relative path to working directory
+import os
 path_to_detector = os.environ.get("FCCDETECTORS", "")
 print(path_to_detector)
 detectors_to_use=[
-                    'Detector/DetFCCeeIDEA-LAr/compact/FCCee_DectMaster.xml',
+#                    'Detector/DetFCCeeIDEA-LAr/compact/FCCee_DectMaster.xml',
+                    'Detector/DetFCCeeIDEA-LAr/compact/FCCee_DectMaster_thetamodulemerged.xml'
                   ]
 # prefix all xmls with path_to_detector
 geoservice.detectors = [os.path.join(path_to_detector, _det) for _det in detectors_to_use]
 geoservice.OutputLevel = INFO
 
-ecalBarrelReadoutName = "ECalBarrelPhiEta"
+#ecalBarrelReadoutName = "ECalBarrelPhiEta"
+ecalBarrelReadoutName = "ECalBarrelModuleThetaMerged"
+#hcalBarrelReadoutName = "ECalBarrelPhiEta"
+hcalBarrelReadoutName = "HCalBarrelReadout"
 #BarrelNoisePath = "/afs/cern.ch/user/b/brfranco/work/public/LAr_setups/230208/LAr_scripts/geometry/noise_capa_230301/elecNoise_ecalBarrelFCCee.root"
 BarrelNoisePath = os.environ['FCCBASEDIR']+"/LAr_scripts/data/elecNoise_ecalBarrelFCCee.root"
 ecalBarrelNoiseHistName = "h_elecNoise_fcc_"
 
 from Configurables import CellPositionsECalBarrelTool
-# ATTENTION!
-# The parameters have to be default in the tools, problem in Gaudi does not propagate the options through 2 tools
-#ECalBcells = CellPositionsECalBarrelTool("CellPositionsECalBarrel", 
-#                                         readoutName = ecalBarrelReadoutName, 
-#                                         OutputLevel = INFO)
-#ECalBcells = CellPositionsECalBarrelTool("CellPositionsECalBarrel")
+ECalBcells = CellPositionsECalBarrelTool("CellPositionsECalBarrel", 
+                                         readoutName = ecalBarrelReadoutName
+)                                         
+#                                         OutputLevel = DEBUG)
+#print(ECalBcells)
 
-from Configurables import CreateFCChhCaloNoiseLevelMap, ReadNoiseFromFileTool
-ECalNoiseTool = ReadNoiseFromFileTool("ReadNoiseFromFileToolECal",  
+#from Configurables import CreateFCChhCaloNoiseLevelMap, ReadNoiseFromFileTool
+from Configurables import CreateFCCeeCaloNoiseLevelMap, ReadNoiseFromFileTool
+ECalNoiseTool = ReadNoiseFromFileTool("ReadNoiseFromFileToolECal",
+                                      useSegmentation = False,
+                                      cellPositionsTool = ECalBcells,
                                       readoutName = ecalBarrelReadoutName,
                                       noiseFileName = BarrelNoisePath,
                                       elecNoiseHistoName = ecalBarrelNoiseHistName,
@@ -38,8 +44,9 @@ ECalNoiseTool = ReadNoiseFromFileTool("ReadNoiseFromFileToolECal",
                                       scaleFactor = 1/1000., #MeV to GeV
                                       OutputLevel=DEBUG)
 
-HCalNoiseTool = ReadNoiseFromFileTool("ReadNoiseFromFileToolHCal",  
-                                      readoutName = ecalBarrelReadoutName,
+HCalNoiseTool = ReadNoiseFromFileTool("ReadNoiseFromFileToolHCal",
+                                      useSegmentation = True,
+                                      readoutName = hcalBarrelReadoutName,
                                       noiseFileName = BarrelNoisePath,
                                       elecNoiseHistoName = ecalBarrelNoiseHistName,
                                       setNoiseOffset = False,
@@ -49,20 +56,21 @@ HCalNoiseTool = ReadNoiseFromFileTool("ReadNoiseFromFileToolHCal",
                                       scaleFactor = 1/1000., #MeV to GeV
                                       OutputLevel=DEBUG)
 
-noisePerCell = CreateFCChhCaloNoiseLevelMap("noisePerCell", 
+#noisePerCell = CreateFCChhCaloNoiseLevelMap("noisePerCell",
+noisePerCell = CreateFCCeeCaloNoiseLevelMap("noisePerCell", 
                                             ECalBarrelNoiseTool = ECalNoiseTool, 
                                             ecalBarrelSysId = 4,
                                             HCalBarrelNoiseTool = HCalNoiseTool,
-                                            readoutNamesPhiEta=[ecalBarrelReadoutName],
-                                            systemNamesPhiEta=["system"],
-                                            systemValuesPhiEta=[4],
-                                            activeFieldNamesPhiEta=["layer"],
+                                            readoutNamesModuleTheta=[ecalBarrelReadoutName],
+                                            systemNamesModuleTheta=["system"],
+                                            systemValuesModuleTheta=[4],
+                                            activeFieldNamesModuleTheta=["layer"],
                                             activeVolumesNumbers = [12],
                                             #activeVolumesEta = [1.2524, 1.2234, 1.1956, 1.1561, 1.1189, 1.0839, 1.0509, 0.9999, 0.9534, 0.91072],
                                             readoutNamesVolumes=[],
                                             outputFileName="cellNoise_map_electronicsNoiseLevel.root",
                                             OutputLevel=DEBUG)
-
+#noisePerCell.ECalBarrelNoiseTool.cellPositionsTool.readoutName = ecalBarrelReadoutName
 
 # ApplicationMgr
 from Configurables import ApplicationMgr
